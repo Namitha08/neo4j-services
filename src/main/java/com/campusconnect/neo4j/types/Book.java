6efaadb55bbcdbf6b5dc0324ca9fdcd19fe1594e
Shivakumara Narayanaswamy <Shivakumara_Narayanaswamy@intuit.com>
2015-02-16 22:48:53 +0530

    -Adding book resource files

---
+++ src/main/java/com/campusconnect/neo4j/types/Book.java
@@ -0,0 +1,52 @@
+package com.campusconnect.neo4j.types;
+
+import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.Indexed;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
+/**
+ * Created by sn1 on 2/16/15.
+ */
+@NodeEntity
+public class Book {
+    @GraphId
+    private Long nodeId;
+
+    @Indexed(unique=true)
+    private String id;
+    
+    private String name;
+    private String isbn;
+
+    public Long getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(Long nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
